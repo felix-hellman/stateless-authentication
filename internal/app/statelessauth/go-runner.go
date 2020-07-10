e1f934c1dafa1fd77ca2261b86467a15fd70086e
felix-hellman <felixhellman.ro@gmail.com>
2020-07-10 11:12:54 +0200

    Can connect to psql database

--- internal/app/statelessauth/go-runner.go
+++ internal/app/statelessauth/go-runner.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/pascaldekloe/jwt"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "test"
+	password = "test"
+	dbname   = "postgres"
+)
+
 func main() {
+
 	configuration, error := loadConfig("./configuration.yaml")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	if error != nil {
 		log.Fatal(error)
 	}
